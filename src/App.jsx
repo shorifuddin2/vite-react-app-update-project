69422df61c904a44a68ed3c7274d5c6e04316034
shori <shorifuddin240915@gmail.com>
2023-05-04 15:57:46 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+
+
+const App = () => {
+  return (
+    <div>
+        
+
+        <div className="card w-96 bg-base-100 shadow-xl">
+  <figure><img src="https://images.unsplash.com/photo-1542291026-7eec264c27ff?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8c2hvZXN8ZW58MHx8MHx8&w=1000&q=80" alt="Shoes" /></figure>
+  <div className="card-body">
+    <h2 className="card-title">Shoes!</h2>
+    <p>If a dog chews shoes whose shoes does he choose?</p>
+    <div className="card-actions justify-end">
+      <button className="btn btn-primary">Buy Now</button>
+    </div>
+  </div>
+</div>
+    </div>
+  );
+};
+
+export default App;
